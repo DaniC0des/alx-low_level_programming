ca585e8d5268005574643cf3c0e67f483d178215
Ogbu Chukwuemeka <danielini95@gmail.com>
2022-03-16 21:42:28 -0700

    task 11

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,29 +1,18 @@
 #include <stdio.h>
 /**
- * main - computes and prints all the multiples
- * of 2 or 5 below 1024
+ * main - check for multiples of 3 and 5
  * Return: 0
  */
 int main(void)
 {
-	unsigned long int sum3, sum5, sum;
-	int i;
-
-	sum3 = 0;
-	sum5 = 0;
-	sum = 0;
-
-	for (i = 0; i < 1024; ++i)
+	int x = 1024, y, sum = 0;
+	for (y = 0; y < x; y++)
 	{
-		if ((i % 3) == 0)
+		if ((y % 3 == 0) || (y % 5 == 0))
 		{
-			sum3 = sum3 + 1;
-		} else if ((i % 5) == 0)
-		{
-			sum5 = sum5 + 1;
+			sum = sum + y;
 		}
 	}
-	sum = sum3 + sum5;
-	printf("%lu\n", sum);
+	printf("%d\n", sum);
 	return (0);
 }
